95849552b44fc1ff1a3c9e4a78552ba3af22ceee
Paige0909 <peichun0909@gmail.com>
2025-12-09 13:18:46 +0800

    Update manage.py

--- part3/Pendulum/manage.py
+++ part3/Pendulum/manage.py
@@ -132,15 +132,6 @@ class Experiment:
     def close(self):
         self.env.close()
 
-if __name__=="__main__":
-    env_wrapper=PendulumEnvWrapper(render_mode="human")
-    agent=RandomAgent(action=env_wrapper.action, max_action=env_wrapper.max_action)
-    exper=Experiment(env=env_wrapper, agent=agent, episode_len=10)
-    for ep in range(5):
-        total_reward=exper.run_episode(render=True)
-        print(f"Episode {ep+1} total reward: {total_reward:.2f}")
-
-    exper.close()
 
 
 
